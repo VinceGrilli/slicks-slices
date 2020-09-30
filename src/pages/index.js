cce6ab4ee7c09a994cf96a892105de672bfea294
BrainJarProductions <vgrilli1@gmail.com>
2020-09-30 15:34:26 -0400

    add layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import Nav from '../components/Nav';
 
 export default function HomePage() {
   return (
-    <div>
-      <Nav />
+    <>
       <p>Hey Im the f'n home page</p>
-    </div>
+      <p>sencond element</p>
+    </>
   );
 }
--- src/pages/pizzas.js
+++ src/pages/pizzas.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function PizzasPage() {
   return (
-    <div>
+    <>
       <p>Hey Im the f'n Pizzas page</p>
-    </div>
+    </>
   );
 }
